b8b2a528a5bc6a7a23e6b6d6effd6cece46eecc2
Tiago Ferreira <tiagorf@gmail.com>
2017-12-29 14:44:44 -0600

    Updated with usage info

--- Get-Page-ResponseTime.ps1
+++ Get-Page-ResponseTime.ps1
@@ -1,4 +1,7 @@
-﻿param($URL, $Times)
+﻿## Script to check a URL Response time
+## Example usage: Get-Page-ResponseTime.ps1 -times 10 -url http://xx
+
+param($URL, $Times)
 $i = 0
 While ($i -lt $Times)
 {$Request = New-Object System.Net.WebClient
@@ -7,5 +10,5 @@ $Start = Get-Date
 $PageRequest = $Request.DownloadString($URL)
 $TimeTaken = ((Get-Date) - $Start).TotalMilliseconds 
 $Request.Dispose()
-Write-Host Request $i took $TimeTaken ms -ForegroundColor Green
+Write-Host Request $i took $TimeTaken ms -ForegroundColor Red
 $i ++}
